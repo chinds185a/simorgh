23ca3d02231a244b9c21a133147e45abdb9cc0ee
Chris Hinds <chris.hinds@bbc.co.uk>
2018-10-22 10:12:53 +0100

    Pass history object to AMP Dicument renderer to intercept push events

--- src/app/containers/ArchivedArticle/index.jsx
+++ src/app/containers/ArchivedArticle/index.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
 import { shape } from 'prop-types';
 import AMPDocument from 'react-amp-document';
 import Header from '../../components/Header';
@@ -7,20 +8,25 @@ import Footer from '../Footer';
 /*
   [1] This handles async data fetching, and a 'loading state', which we should look to handle more intelligently.
 */
-const ArcivedArticleContainer = ({ data }) => (
-  <Fragment>
-    <Header />
-    <AMPDocument src={data.ampUrl} />
-    <Footer />
-  </Fragment>
-);
+const ArcivedArticleContainer = ({ data, history }) => {
+  console.log(data.ampUrl);
+  return (
+    <Fragment>
+      <Header />
+      <AMPDocument src={data.ampUrl} router={history} />
+      <Footer />
+    </Fragment>
+  );
+};
 
 ArcivedArticleContainer.propTypes = {
   data: shape,
+  history: shape,
 };
 
 ArcivedArticleContainer.defaultProps = {
   data: null,
+  history: null,
 };
 
-export default ArcivedArticleContainer;
+export default withRouter(ArcivedArticleContainer);
